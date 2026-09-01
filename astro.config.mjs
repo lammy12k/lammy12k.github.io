efae5ddea73e3961bcdb8c7ac5220f370ccf2ad4
Kyle <kylef@myyahoo.com>
2026-09-01 18:16:43 -0400

    Initial Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,35 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import sitemap from '@astrojs/sitemap';
+import astroExpressiveCode from 'astro-expressive-code';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://lammy12k.github.io',
+	
+	integrations: [
+		astroExpressiveCode({
+			themes: ['github-dark'],
+		}),
+		sitemap()
+	],
+	
+	vite: {
+		plugins: [
+			tailwindcss(),
+			ViteImageOptimizer({
+				png: { quality: 80 },
+				jpeg: { quality: 75 },
+				webp: { quality: 80 },
+				avif: { quality: 70 },
+				svg: {
+					plugins: [
+						{ name: 'removeViewBox', active: false },
+						{ name: 'sortAttrs', active: true }
+					]
+				}
+			})
+		],
+	}
+});
